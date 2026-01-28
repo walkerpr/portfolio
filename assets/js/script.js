45a7b8fca12c3aefb46fd4e8240515b3f3aa4365
walkerpr <walkerpatrickr@protonmail.com>
2026-01-27 18:45:06 -0800

    Feat: remove unused JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -154,24 +154,6 @@ for (let i = 0; i < navigationLinks.length; i++) {
   });
 }
 
-
-
-var coll = document.getElementsByClassName("collapsible_btn");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
-
-
 const collapsible = document.getElementsByClassName("collapsible");
 
 for (let i = 0; i < collapsible.length; i++) {
