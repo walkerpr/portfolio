724fbd9321d7f431dd17506cc7da79cba769dd8b
walkerpr <walkerpatrickr@protonmail.com>
2026-02-23 22:55:02 -0800

    refactor

--- js/script.js
+++ js/script.js
@@ -18,7 +18,6 @@ for (let i = 0; i < collapsible.length; i++) {
 // testimonials variables
 const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
 const modalContainer = document.querySelector("[data-modal-container]");
-const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
 const overlay = document.querySelector("[data-overlay]");
 
 // modal variable
@@ -47,11 +46,6 @@ for (let i = 0; i < testimonialsItem.length; i++) {
     testimonialsModalFunc();
   });
 }
-
-// add click event to modal close button
-modalCloseBtn.addEventListener("click", testimonialsModalFunc);
-overlay.addEventListener("click", testimonialsModalFunc);
-
 
 // custom select variables
 const selectData = document.querySelector("[data-select]");
